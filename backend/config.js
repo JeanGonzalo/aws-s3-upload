2212d0768697b5aa57ec257dd13e3056d9e393a9
JeanGonzalo <rjeangonzalo@gmail.com>
2021-12-23 14:35:02 -0500

    first commit

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+exports.globals = {
+    "AWS-BUCKET-NAME": "tgvideos-bucket", // Replace with your bucket 
+    "ACCESS-ID": "", // Replace with your access key id 
+    "AWS-SECRET-KEY": "", // Replace with your secret access key 
+    "VERSION": "v4" 
+}
