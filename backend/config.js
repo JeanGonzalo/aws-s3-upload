6624e020feb0fbde27e318916fd8f8abfe1d22df
JeanGonzalo <rjeangonzalo@gmail.com>
2022-01-03 12:55:37 -0500

    ammm

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 exports.globals = {
     "AWS-BUCKET-NAME": "tgvideos-bucket", // Replace with your bucket 
-    "ACCESS-ID": "", // Replace with your access key id 
-    "AWS-SECRET-KEY": "", // Replace with your secret access key 
+    "ACCESS-ID": "AKIARHL5SGVJOMP4VRXA", // Replace with your access key id 
+    "AWS-SECRET-KEY": "VH67kZSEXrXjIzBViTwfc0z/gXjKALJVKrsnzcAj", // Replace with your secret access key 
     "VERSION": "v4" 
 }
